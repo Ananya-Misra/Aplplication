6d462de7876421b24f8b018564c99d3c12b9c09c
Ananya Misra <anumisra2@gmail.com>
2021-05-04 21:11:46 +0530

    add view pager

--- app/src/main/java/com/example/layoutapplication/MainActivity.kt
+++ app/src/main/java/com/example/layoutapplication/MainActivity.kt
@@ -5,15 +5,33 @@ import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewapp.Adapter
+import kotlinx.android.synthetic.main.pagerview_layout.*
 import kotlinx.android.synthetic.main.recyclerview_layout.*
+import me.relex.circleindicator.CircleIndicator
+import me.relex.circleindicator.CircleIndicator3
 
-class MainActivity : AppCompatActivity() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    var titleList= mutableListOf<String>()
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.biodata_of_man)
+        setContentView(R.layout.pagerview_layout)
 
+        postToList()
+        view_pager.adapter=ViewPagerAdapter(titleList)
 
+        val indicator=findViewById<CircleIndicator3>(R.id.indicator)
+        indicator.setViewPager(view_pager)
+    }
+
+    private fun addToList(topic:String){
+        titleList.add(topic)
+
+    }
+    private fun postToList(){
+        for(i in 1..5){
+            addToList("Topic $i")
+        }
     }
 }
