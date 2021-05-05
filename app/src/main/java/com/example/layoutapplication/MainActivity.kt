e1212be4131565717abf2f53e9bc2500900cdadd
Ananya Misra <anumisra2@gmail.com>
2021-05-05 21:55:32 +0530

    inflating a layout on button click

--- app/src/main/java/com/example/layoutapplication/MainActivity.kt
+++ app/src/main/java/com/example/layoutapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.layoutapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.RelativeLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewapp.Adapter
@@ -16,14 +20,22 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pagerview_layout)
+        setContentView(R.layout.activity_main0)
+
+        val layoutToAdd = findViewById<View>(R.id.rlVip) as RelativeLayout
+        val button = findViewById<View>(R.id.btn_showDialog) as Button
+        button.setOnClickListener {
+            val inflater = LayoutInflater
+                    .from(applicationContext)
+            val view = inflater.inflate(R.layout.pagerview_layout, null)
+            layoutToAdd!!.addView(view)
 
         postToList()
         view_pager.adapter=ViewPagerAdapter(titleList)
 
         val indicator=findViewById<CircleIndicator3>(R.id.indicator)
         indicator.setViewPager(view_pager)
-    }
+    }}
 
     private fun addToList(topic:String){
         titleList.add(topic)
