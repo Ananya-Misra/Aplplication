fe62f04d57a4b16b02b73f5029d68ac840cade3d
Ananya Misra <anumisra2@gmail.com>
2021-05-06 20:42:01 +0530

    applying popup window

--- app/src/main/java/com/example/layoutapplication/MainActivity.kt
+++ app/src/main/java/com/example/layoutapplication/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.PopupMenu
+import android.widget.PopupWindow
 import android.widget.RelativeLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,26 +19,29 @@ import me.relex.circleindicator.CircleIndicator3
 
 class MainActivity : AppCompatActivity()
 {
+    private var popup: PopupWindow? = null
     var titleList= mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main0)
 
-        val layoutToAdd = findViewById<View>(R.id.rlVip) as RelativeLayout
-        val button = findViewById<View>(R.id.btn_showDialog) as Button
-        button.setOnClickListener {
-            val inflater = LayoutInflater
-                    .from(applicationContext)
-            val view = inflater.inflate(R.layout.pagerview_layout, null)
-            layoutToAdd!!.addView(view)
 
-        postToList()
-        view_pager.adapter=ViewPagerAdapter(titleList)
+        val popupButton =
+            findViewById<Button>(R.id.btn_showDialog)
+        popupButton.setOnClickListener { v ->
+            val popUpClass = PopupClass()
+            popUpClass.showPopupWindow(v)
 
-        val indicator=findViewById<CircleIndicator3>(R.id.indicator)
-        indicator.setViewPager(view_pager)
-    }}
+
+
+            postToList()
+            view_pager.adapter = ViewPagerAdapter(titleList)
+
+            val indicator = findViewById<CircleIndicator3>(R.id.indicator)
+            indicator.setViewPager(view_pager)
+        }
+    }
 
     private fun addToList(topic:String){
         titleList.add(topic)
