bb49673c8ea735f26908907cf4efe883fd6a3fdd
svan0004 <siddarth.vanakudari@synchronoss.com>
2021-05-09 19:53:18 +0530

    fixed view pager

--- app/src/main/java/com/example/layoutapplication/CustomPagerAdapter.java
+++ app/src/main/java/com/example/layoutapplication/CustomPagerAdapter.java
@@ -1,18 +1,22 @@
 package com.example.layoutapplication;
-        import android.content.Context;
-        import android.text.TextUtils;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ImageView;
-        import android.widget.TextView;
 
-        import androidx.viewpager.widget.PagerAdapter;
-        import androidx.viewpager.widget.ViewPager;
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-        import java.util.ArrayList;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class CustomPagerAdapter extends PagerAdapter {
+
+    int[] bioLayouts = {R.layout.profile_of_woman, R.layout.profile_of_man
+            , R.layout.profile, R.layout.biodata_of_man};
 
     Context context;
     ArrayList<Integer> pager;
@@ -42,7 +46,7 @@ public class CustomPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return 4;
+        return bioLayouts.length;
     }
 
     @Override
@@ -54,23 +58,7 @@ public class CustomPagerAdapter extends PagerAdapter {
 
         LayoutInflater inflater = (LayoutInflater) collection.getContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        int resId = 0;
-        switch (position) {
-            case 0:
-                resId = R.layout.profile_of_woman;
-                break;
-            case 1:
-                resId = R.layout.profile_of_man;
-                break;
-            case 2:
-                resId = R.layout.profile;
-                break;
-            case 3:
-                resId = R.layout.biodata_of_man;
-                break;
-
-        }
-
+        int resId = bioLayouts[position];
         View view = inflater.inflate(resId, null);
         setViews(view);
 
@@ -81,23 +69,23 @@ public class CustomPagerAdapter extends PagerAdapter {
 
 
     private void setViews(View view) {
-        imgProfile=view.findViewById(R.id.imgProfile);
+        imgProfile = view.findViewById(R.id.imgProfile);
         tvName = view.findViewById(R.id.tvFullName);
-        tvAddressInfo=view.findViewById(R.id.tvAddressInfo);
-        tvAgeInfo=view.findViewById(R.id.tvAgeInfo);
+        tvAddressInfo = view.findViewById(R.id.tvAddressInfo);
+        tvAgeInfo = view.findViewById(R.id.tvAgeInfo);
         tvAboutInfo = view.findViewById(R.id.tvAboutInfo);
-        tvBloodGroupInfo=view.findViewById(R.id.tvBloodGroupInfo);
+        tvBloodGroupInfo = view.findViewById(R.id.tvBloodGroupInfo);
         tvCallInfo = view.findViewById(R.id.tvCallInfo);
         tvDOBInfo = view.findViewById(R.id.tvDOBInfo);
         tvEmailInfo = view.findViewById(R.id.tvEmailInfo);
-        tvEducationInfo=view.findViewById(R.id.tvEducationInfo);
-        tvFamilyInfo=view.findViewById(R.id.tvFamilyInfo);
-        tvExpectationsInfo=view.findViewById(R.id.tvExpecationsInfo);
-        tvHobbiesInfo=view.findViewById(R.id.tvHobbiesInfo);
-        tvHeightInfo=view.findViewById(R.id.tvHeightInfo);
-        tvLanguageInfo=view.findViewById(R.id.tvLanguageInfo);
+        tvEducationInfo = view.findViewById(R.id.tvEducationInfo);
+        tvFamilyInfo = view.findViewById(R.id.tvFamilyInfo);
+        tvExpectationsInfo = view.findViewById(R.id.tvExpecationsInfo);
+        tvHobbiesInfo = view.findViewById(R.id.tvHobbiesInfo);
+        tvHeightInfo = view.findViewById(R.id.tvHeightInfo);
+        tvLanguageInfo = view.findViewById(R.id.tvLanguageInfo);
         tvWeightInfo = view.findViewById(R.id.tvWeightInfo);
-        tvTimeInfo=view.findViewById(R.id.tvTimeInfo);
+        tvTimeInfo = view.findViewById(R.id.tvTimeInfo);
 
 
     }
