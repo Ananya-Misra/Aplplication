9e5a11c07331d71462413f66d00eac210d282138
Ananya Misra <anumisra2@gmail.com>
2021-05-13 19:24:03 +0530

    added another layout

--- app/src/main/java/com/example/layoutapplication/MainActivity.java
+++ app/src/main/java/com/example/layoutapplication/MainActivity.java
@@ -1,26 +1,19 @@
-package com.example.layoutapplication;
+ package com.example.layoutapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-
-
-import android.text.Html;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.LinearLayout;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher_activity);
         // Create a button handler and call the dialog box display method in it
+
         Button popupButton = findViewById(R.id.btn_showDialog);
         popupButton.setOnClickListener(new View.OnClickListener() {
 
--- app/src/main/java/com/example/layoutapplication/PopUpClass.java
+++ app/src/main/java/com/example/layoutapplication/PopUpClass.java
@@ -92,7 +92,7 @@ public class PopUpClass {
     }
 
     public void addDot(int page_position, Context context) {
-        dot = new TextView[7];
+        dot = new TextView[8];
         layout_dot.removeAllViews();
 
         for (int i = 0; i < dot.length; i++) {
